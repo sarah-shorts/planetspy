20fadecffb602eed0be08c5545daee612b25ae15
sarah schwarz <sarahschwarza@gmail.com>
2019-04-07 01:17:32 -0700

    debug

--- planetspy/engines/_pp.py
+++ planetspy/engines/_pp.py
@@ -15,17 +15,26 @@ class PP(SimulationEngine):
             z_dist = np.zeros((num, num))
             for i in range(num):
                 padargs = [(0, num - i), 'constant']
+                """
                 x_dist[i] = np.pad((bodyref[:i,0]-bodyref[i,0]), *padargs, constant_values = 0)
                 y_dist[i] = np.pad((bodyref[:i,1]-bodyref[i,1]), *padargs, constant_values = 0)
                 z_dist[i] = np.pad((bodyref[:i,2]-bodyref[i,2]), *padargs, constant_values = 0)
                 distsq[i] = np.pad((x_dist[i,:i]**2 + y_dist[i,:i]**2 + z_dist[i,:i]**2), *padargs, constant_values = 0)
                 dist[i] = np.pad(np.sqrt(distsq[i,:i]), *padargs, constant_values = 0)
                 distsq[i] = np.pad(1/distsq[i,:i], *padargs, constant_values = 0)
-            dist += dist.T
-            distsq += distsq.T
-            x_dist -= x_dist.T
-            y_dist -= y_dist.T
-            z_dist -= z_dist.T
+                """
+                x_dist[i] = (bodyref[::,0]-bodyref[i,0])
+                y_dist[i] = (bodyref[::,1]-bodyref[i,1])
+                z_dist[i] = (bodyref[::,2]-bodyref[i,2])
+                distsq[i] = (x_dist[i,::]**2 + y_dist[i,::]**2 + z_dist[i,::]**2)
+                dist[i] = np.sqrt(distsq[i,::])
+                distsq[i] = np.where(distsq[i] != 0, distsq[i], np.inf)
+                distsq[i] = 1/distsq[i]
+            #dist += dist.T
+            #distsq += distsq.T
+            #x_dist -= x_dist.T
+            #y_dist -= y_dist.T
+            #z_dist -= z_dist.T
             acceltot = self.G*np.hstack(bodyref[::,7])*distsq
             dist = np.where(dist != 0, dist, np.inf)
             accel_x = np.sum((acceltot*(x_dist/dist)), axis=1)
@@ -34,9 +43,9 @@ class PP(SimulationEngine):
             dvx = accel_x * self.steptime
             dvy = accel_y * self.steptime
             dvz = accel_z * self.steptime
-            bodyref[::,0] += (bodyref[::,3] + dvx/2)*self.steptime
-            bodyref[::,1] += (bodyref[::,4] + dvy/2)*self.steptime
-            bodyref[::,2] += (bodyref[::,5] + dvz/2)*self.steptime
+            bodyref[::,0] += (bodyref[::,3] + dvx)*self.steptime
+            bodyref[::,1] += (bodyref[::,4] + dvy)*self.steptime
+            bodyref[::,2] += (bodyref[::,5] + dvz)*self.steptime
             bodyref[::,3] += dvx
             bodyref[::,4] += dvy
             bodyref[::,5] += dvz
