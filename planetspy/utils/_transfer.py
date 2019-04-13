553502d254cdf48eec37a8f6fb273a2b2cdf7224
sarah schwarz <sarahschwarza@gmail.com>
2019-04-12 18:03:19 -0700

    starting hohmann transfer and whatever aled did idk

---
+++ planetspy/utils/_transfer.py
@@ -0,0 +1,14 @@
+from numpy import *
+G = 6.67*10** -11
+
+def delta_v(mass_p, r_parent, r_target):
+    global G
+    return np.sqrt(G*mass_p/r_parent)*(np.sqrt(2*r_target/(r_parent+r_target))-1)
+def time_transfer(mass_p, r_parent, r_target):
+    global G
+    return np.pi*np.sqrt((r_parent+r_target)**3/(8*G*mass_p))
+def angular_alignment(r_parent, r_target):
+    global G
+    return np.pi*(1-1/np.sqrt(8)*(r_parent/r_target+1)**3/2)
+def calc_hohmann():
+    
