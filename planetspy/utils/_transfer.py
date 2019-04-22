3409f4cef568720ce57728bb5e3ee954685395fc
sarah schwarz <sarahschwarza@gmail.com>
2019-04-22 11:09:48 -0700

    meh

--- planetspy/utils/_transfer.py
+++ planetspy/utils/_transfer.py
@@ -1,14 +1,12 @@
 from numpy import *
-G = 6.67*10** -11
+from scipy.optimize import minimize
+G = 6.67*10**-11
 
 def delta_v(mass_p, r_parent, r_target):
-    global G
     return np.sqrt(G*mass_p/r_parent)*(np.sqrt(2*r_target/(r_parent+r_target))-1)
 def time_transfer(mass_p, r_parent, r_target):
-    global G
     return np.pi*np.sqrt((r_parent+r_target)**3/(8*G*mass_p))
 def angular_alignment(r_parent, r_target):
-    global G
     return np.pi*(1-1/np.sqrt(8)*(r_parent/r_target+1)**3/2)
-def calc_hohmann():
-    
+def calc_hohmann_minV():
+    return minimize(delta_v, )
